a630a24b81662fd09eee82589dc7bbda83cae830
Alina Domnich <78098869+Alina-d87@users.noreply.github.com>
2022-07-29 18:53:58 +0300

    work task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+console.log(refs);
+
+let timerId = null;
+
+refs.btnStart.addEventListener('click', onStartClick);
+refs.btnStop.addEventListener('click', onStopClick);
+
+function onStartClick(e) {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  timerId = setTimeout(onStartClick, 1000);
+  if (onStartClick) {
+    refs.btnStart.disabled = true;
+  }
+}
+
+function onStopClick(e) {
+  clearTimeout(timerId);
+  if (onStopClick) {
+    refs.btnStart.disabled = false;
+  }
+}
